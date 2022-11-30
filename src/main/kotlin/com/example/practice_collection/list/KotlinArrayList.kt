507866642ca2070c6fac915adf6d4d5c1f7562ce
ParkJiwoon <bcp0109@gmail.com>
2022-11-30 21:34:04 +0900

    ✨ KotlinArrayList 구현

--- src/main/kotlin/com/example/practice_collection/list/KotlinArrayList.kt
+++ src/main/kotlin/com/example/practice_collection/list/KotlinArrayList.kt
@@ -1,31 +1,117 @@
 package com.example.practice_collection.list
 
-class KotlinArrayList<E> : JavaList<E> {
+import kotlin.math.max
+import kotlin.math.min
+
+class KotlinArrayList<E>(
+    private var elements: Array<Any?> = emptyArray(),
+    private var size: Int = 0
+) : JavaList<E> {
+
+    companion object {
+        private const val DEFAULT_CAPACITY: Int = 10
+    }
+
     override fun size(): Int {
-        TODO("Not yet implemented")
+        return this.size
     }
 
     override fun add(e: E) {
-        TODO("Not yet implemented")
+        if (size == elements.size) {
+            growElements(size / 2)
+        }
+
+        this.elements[size] = e;
+        this.size += 1
     }
 
     override fun get(index: Int): E {
-        TODO("Not yet implemented")
+        if (index < 0 || index >= size) {
+            throw IndexOutOfBoundsException()
+        }
+
+        return elements[index] as E
     }
 
     override fun remove(e: E) {
-        TODO("Not yet implemented")
+        val targetIndex = indexOfElement(e)
+
+        if (targetIndex == -1) {
+            return
+        }
+
+        if (targetIndex != size - 1) {
+            val newElements = arrayOfNulls<Any>(size)
+
+            (0 until targetIndex).forEach { newElements[it] = elements[it] }
+            (targetIndex until (size - 1)).forEach { newElements[it] = elements[it + 1] }
+
+            this.elements = newElements
+        }
+
+        this.elements[size - 1] = null
+        this.size -= 1
     }
 
     override fun contains(e: E): Boolean {
-        TODO("Not yet implemented")
+        return indexOfElement(e) > -1
     }
 
-    override fun addAll(list: JavaList<E>?) {
-        TODO("Not yet implemented")
+    override fun addAll(list: JavaList<E>) {
+        if (list.size() == 0) {
+            return
+        }
+
+        val newSize = size + list.size()
+        growElements(newSize)
+
+        for (i in size until newSize) {
+            elements[i] = list[i - size]
+        }
+
+        size = newSize
+    }
+
+    private fun growElements(capacity: Int) {
+        if (size == 0) {
+            this.elements = copyOf(elements, max(capacity, DEFAULT_CAPACITY))
+            return
+        }
+
+        val oldCapacity = elements.size
+        val newCapacity = oldCapacity + capacity
+        this.elements = copyOf(elements, newCapacity)
+    }
+
+    private fun copyOf(array: Array<Any?>, newSize: Int): Array<Any?> {
+        val newArray = arrayOfNulls<Any>(newSize)
+
+        val size = min(array.size, newArray.size)
+        (0 until size).forEach { newArray[it] = array[it] }
+
+        return newArray
+    }
+
+    private fun indexOfElement(e: E?): Int {
+        for (i in 0 until size) {
+            if (e == null) {
+                if (elements[i] == null) {
+                    return i
+                }
+
+                continue
+            }
+
+            if (e == elements[i]) {
+                return i
+            }
+        }
+
+        return -1
     }
 
     override fun toArray(): Array<E> {
         TODO("Not yet implemented")
     }
 }
+
